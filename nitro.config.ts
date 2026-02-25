4e11df8e571aae7b01d302559b720581d6a8b6e1
te <te1@users.noreply.github.com>
2026-02-25 21:46:04 +0100

    attempt to make a full static site with nitro

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,8 +1,9 @@
-import { defineNitroConfig } from "nitropack/config"
+import { defineNitroConfig } from "nitropack/config";
 
 // https://nitro.build/config
 export default defineNitroConfig({
   compatibilityDate: "latest",
   srcDir: "server",
-  imports: false
+  imports: false,
+  serveStatic: true,
 });
